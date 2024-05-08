778ad6aa1fda73315236d5cc0aa8d870df289b93
Vap0r404 <130596032+Vap0r404@users.noreply.github.com>
2024-05-08 12:50:33 +0200

    updated snakegame.py
    fixed the not working upper limit for target length

--- snakegame.py
+++ snakegame.py
@@ -194,7 +194,7 @@ def main():
                     elif event.key == pygame.K_r:
                         snake.reset()
                         speed_multiplier = 1  # Reset speed multiplier
-                        target_length = random.randint(10, 20)  # Random target length
+                        target_length = random.randint(10, 50)  # Random target length
                         game_over = False
                         win = False
 
